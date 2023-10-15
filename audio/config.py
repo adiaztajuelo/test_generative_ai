df3898ce6e8edb29d098946b7a545285578ab0d5
Diaz Tajuelo <adrian.diaz-tajuelo@capgemini.com>
2023-10-16 00:48:58 +0200

    update audio transcribe

--- audio/config.py
+++ audio/config.py
@@ -2,3 +2,10 @@ short_audio_mp3 = "data_audio\short_mundial.mp3"
 short_audio_wave = "data_audio/short_mundial_wave.wav"
 short_audio_wave_name = "short_mundial_wave.wav"
 temp_wave_1min = "data_audio/temp.wav"
+output_file = "output/transcription.txt"
+audio_mundial_complete = 'data_audio/final_mundial_2010.mp3'
+audio_segunda_parte = "data_audio/segunda-parte-final-mundial-2010.mp3"
+audio_segunda_parte_wave = "data_audio/segunda-parte-final-mundial-2010_wave.wav"
+audio_prorroga_parte_wave = "data_audio/prorroga-final-mundial-2010_wave.wav"
+output_transcription_complete = "output/transcription_complete.txt"
+
--- audio/test_audio_transcribe.py
+++ audio/test_audio_transcribe.py
@@ -1,21 +1,42 @@
+from config import audio_prorroga_parte_wave, temp_wave_1min, output_file, short_audio_mp3, output_transcription_complete
+from pydub import AudioSegment
 import whisper
-from whisper.utils import get_writer
-from config import short_audio_mp3, temp_wave_1min
-import os
-
-model = whisper.load_model("base")
-audio = short_audio_mp3
-print(audio)
-audio_data = whisper.load_audio(temp_wave_1min)
-result = model.transcribe(temp_wave_1min)
-output_directory = "./"
 
 
-# Save as a TXT file without any line breaks
-with open("transcription_whisper.txt", "w", encoding="utf-8") as txt:
-    txt.write(result["text"])
 
 
-# Save as a TXT file with hard line breaks
-txt_writer = get_writer("txt", output_directory)
-txt_writer(result, audio)
+# Función para dividir el archivo de data_audio en segmentos
+def split_audio_and_transcribe(file_path, max_duration):
+    sound = AudioSegment.from_wav(file_path)
+    # sound = AudioSegment.from_mp3(file_path)
+    start_time = 0
+    model = whisper.load_model("base")
+
+    while start_time < len(sound):
+        end_time = min(start_time + max_duration, len(sound))
+        segment = sound[start_time:end_time]
+
+        # Guarda el segmento como archivo temporal
+        segment.export(temp_wave_1min, format="wav")
+
+        # transcribe
+        transcribe_result = model.transcribe(temp_wave_1min)
+
+        with open(output_transcription_complete, "a", encoding="utf-8") as txt:
+            txt.write(transcribe_result["text"])
+
+        transcription_data = transcribe_result["text"]
+        print(transcription_data)
+
+        # Actualiza el inicio para el próximo segmento
+        start_time = end_time
+
+
+
+audio_file_path = audio_prorroga_parte_wave
+
+# Duración máxima de cada parte en milisegundos
+max_duration_per_segment = 60000  # 60 segundos
+# Llama a la función para transcribir el archivo de data_audio por partes
+split_audio_and_transcribe(audio_file_path, max_duration_per_segment)
+
