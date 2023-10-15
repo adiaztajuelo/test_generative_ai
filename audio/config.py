a077d87927cd799bc7e9fad6abc102d425023661
Diaz Tajuelo <adrian.diaz-tajuelo@capgemini.com>
2023-10-15 17:48:47 +0200

    first upload

---
+++ audio/config.py
@@ -0,0 +1,4 @@
+short_audio_mp3 = "data_audio\short_mundial.mp3"
+short_audio_wave = "data_audio/short_mundial_wave.wav"
+short_audio_wave_name = "short_mundial_wave.wav"
+temp_wave_1min = "data_audio/temp.wav"
